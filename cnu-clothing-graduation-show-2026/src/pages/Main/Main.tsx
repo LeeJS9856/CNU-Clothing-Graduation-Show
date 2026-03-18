bd2bffadaf3c8d61365e19e019421e7186371e6d
LeeJS9856 <wnstj1057@naver.com>
2026-03-19 01:32:37 +0900

    fix : 안쓰는 import 삭제

--- cnu-clothing-graduation-show-2026/src/pages/Main/Main.tsx
+++ cnu-clothing-graduation-show-2026/src/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { responsive, responsiveCss } from '@/styles/responsive';
+import { responsiveCss } from '@/styles/responsive';
 import { useResponsive } from '@/hooks/useResponsive';
 
 const Main = (): React.JSX.Element => {
