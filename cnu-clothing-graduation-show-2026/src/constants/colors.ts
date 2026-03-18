55af998c782c9094f710145a81b86b1f3cfe5364
LeeJS9856 <wnstj1057@naver.com>
2026-03-18 23:42:06 +0900

    design : 컬러 상수 설정

---
+++ cnu-clothing-graduation-show-2026/src/constants/colors.ts
@@ -0,0 +1,17 @@
+export const COLORS = {
+  white: '#ffffff',
+  black: '#000000',
+  primary: '#3364A0',
+  background: {
+    main: '#ffffff',
+    grey: '#f0f0f0',
+  },
+  brand: {
+    primary: '#3364A0',
+    secondary: '#163162',
+  },
+  text: {
+    main: '#333333',
+    grey: '#383838',
+  },
+} as const;
