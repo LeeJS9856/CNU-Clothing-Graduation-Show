0a253d2cff879ccf6ffdbe1346ede9589048a88d
ReusCap <nick921@naver.com>
2026-05-18 15:31:21 +0900

    feat:Works화면 구성

--- cnu-clothing-graduation-show-2026/src/App.tsx
+++ cnu-clothing-graduation-show-2026/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import AboutPage from './pages/About/AboutPage';
 import ArchivePage from './pages/Archive/ArchivePage';
+import WorksPage from './pages/Works/WorksPage';
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<AboutPage />} /> {/* About 페이지 경로와 컴포넌트 연결 */}
                 <Route path="/archive" element={<ArchivePage />} /> {/* Archive 페이지 경로와 컴포넌트 연결 */}
+                <Route path="/works" element={<WorksPage />} /> {/* Works 페이지 경로와 컴포넌트 연결 */}
             </Routes>
         </Router>
     );
